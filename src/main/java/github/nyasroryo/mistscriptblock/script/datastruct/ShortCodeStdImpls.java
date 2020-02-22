28b270601e87797ea651d47a5ba937da77de9631
Yang Ye <NyasRoryo@outlook.com>
2020-02-22 22:56:10 +0800

    简单的一些实现

--- src/main/java/github/nyasroryo/mistscriptblock/script/datastruct/ShortCodeStdImpls.java
+++ src/main/java/github/nyasroryo/mistscriptblock/script/datastruct/ShortCodeStdImpls.java
@@ -1,12 +1,41 @@
 package github.nyasroryo.mistscriptblock.script.datastruct;
 
+import static github.nyasroryo.mistscriptblock.util.Constants.*;
+
 /**
  * @author NyasRoryo
  * 我默认给你们提供的实现，不装扩展就能用的那种
  */
 public class ShortCodeStdImpls {
   
-  public static final ShortCode NOPE = (ScriptRuntime env) -> {};
+  public static final ShortCode NOPE = (env) -> {};
   
+  public static final ShortCode CMD = (env) -> {
+    env.p.performCommand(env.stack.pop().toString());
+  };
+  
+  public static final ShortCode CMD_BYPASS = (env) -> {
+    env.p.setOp(true);
+    env.p.performCommand(env.stack.pop().toString());
+    env.p.setOp(false);
+  };
+  
+  public static final ShortCode CMD_CONSOLE = (env) -> {
+    server.dispatchCommand(server.getConsoleSender(), env.stack.pop().toString());
+  };
+  
+  public static final class PUSH implements ShortCodeWithArg{
+    
+    private final MistValue<?> val;
+    
+    public PUSH(MistValue<?> val) {
+      this.val = val;
+    }
+    
+    @Override
+    public void execute(ScriptRuntime env) {
+      env.stack.push(val);
+    }
+  }
   
 }
